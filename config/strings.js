d4714fc9d9b1de5b9b8d2c60f1ce5efe558e0ca0
Devious <74839733+DeviousTheDev@users.noreply.github.com>
2021-01-19 12:43:37 -0500

    Update strings.js

--- config/strings.js
+++ config/strings.js
@@ -1,17 +1,26 @@
 const fs = require("fs");
+const crud = require("./auth/crud.js");
 
-exports.hostname = "[Traumatized@CNC]#~ ";
+exports.hostname = function(name) {
+    if(!name) {
+        name = "CNC";
+    }
+    return "\x1b[33m[Traumatized@" + name + "]#~ ";
+}
 
 exports.Colors = {
-    "Red": "",
-    "Yellow": "",
-    "Blue": "",
-    "Purple": "",
-    "Green": "",
-    "Cyan": "",
-    "Black": "",
-    "Reset": "",
-    "Clear": "\033\143"
+    "Red": "\x1b[31m",
+    "Yellow": "\x1b[33m",
+    "Blue": "\x1b[34m",
+    "Purple": "\x1b[35m",
+    "Green": "\x1b[32m",
+    "Cyan": "\x1b[96m",
+    "Black": "\x1b[30m",
+    "Grey": "\x1b[90m",
+    "Reset": "\x1b[39m",
+    "Clear": "\033[2J\033[1;1H",
+    "Moving_text": "\e[5mBlink",
+    "movingtext2": "\x1b[5mBlink"
 }
 
 exports.CNC_Info = {
