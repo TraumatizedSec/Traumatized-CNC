83fa1a16e3c33ed5601c9e87b8b86f6044702b31
Devious <74839733+DeviousTheDev@users.noreply.github.com>
2021-01-19 14:11:58 -0500

    Update banners.js

--- extra/banners.js
+++ extra/banners.js
@@ -1,9 +1,24 @@
-const config = require("./config/strings.js");
+const fs = require("fs");
+const config = require("../config/strings.js");
+
+exports.main = function() {
+    let main = "";
+    main += config.Colors.Purple + "                  ╔════════════════════════════════════════╗\r\n";
+    main += "                  ║    " + config.Colors.Yellow + "╔╦╗╦═╗╔═╗╦ ╦╔╦╗╔═╗╔╦╗╦╔═╗╔═╗╔╦╗" + config.Colors.Purple + "     ║\r\n";
+    main += "                  ║     " + config.Colors.Yellow + "║ ╠╦╝╠═╣║ ║║║║╠═╣ ║ ║╔═╝║╣  ║║" + config.Colors.Purple + "     ║\r\n";
+    main += "                  ║     " + config.Colors.Yellow + "╩ ╩╚═╩ ╩╚═╝╩ ╩╩ ╩ ╩ ╩╚═╝╚═╝═╩╝" + config.Colors.Purple + "     ║\r\n";
+    main += "                  ╚════════════════════════════════════════╝\r\n" + config.Colors.Reset;
+    return main;
+}
 
 exports.geoBanner = function() {
     let gbnnr = "";
-    gbnnr += "╔═══════════════════════════════╗\r\n";
-    gbnnr += "║      Geo Locator Results      ║\r\n";
-    gbnnr += "╚═══════════════════════════════╝\r\n";
+    gbnnr += config.Colors.Purple + "                       ╔═══════════════════════════════╗\r\n";
+    gbnnr += "                       ║      " + config.Colors.Yellow + "Geo Locator Results" + config.Colors.Purple + "      ║\r\n";
+    gbnnr += "                       ╚═══════════════════════════════╝\r\n" + config.Colors.Purple;
     return gbnnr;
+}
+
+exports.GeoBox = function() {
+    
 }
