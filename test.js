84c0a1bbb1bc9161454b25cc67061be670f2837b
Devious <74839733+DeviousTheDev@users.noreply.github.com>
2021-01-25 15:22:24 -0500

    Update test.js

--- test.js
+++ test.js
@@ -1,59 +1,32 @@
 const fs = require("fs");
 const func = require("./extra/functions.js");
 const crud = require("./auth/crud.js");
+const grid = require("./extra/data_grid");
+const admin = require("./auth/admin.js");
 
 // console.log(gay.show_stats(process.argv[2])); //SHOW STATS
 // console.log(crud.update(process.argv[2], process.argv[3], process.argv[4], process.argv[5], process.argv[6])); // UPDATE USER
 
-console.log("╔════════════╦══════════════╦═════════╗");
-console.log("║ Name       ║ IP           ║ Level   ║");
-console.log("╠════════════╬══════════════╬═════════╣");
-
-var name_box = "  Name      ";
-var name_length = name_box.length;
-
-var ip_box = " IP           ";
-var ip_length = ip_box.length;
-
-var level_box = " Level   ";
-var level_length = level_box.length;
-
-
-
-console.log("║" + fix_name("TEST") + "║" + fix_ip("GAY") + "║");
-console.log("║" + fix_name("nigger") + "║" + fix_ip("skid") + "║");
-console.log("╚═════════════════════════════════════╝");
-
-
-function fix_name(neww) {
-    let new_t = "  " + neww;
-    let new_length = name_length - neww.length - 2;
-    let count = 0;
-    for(count = 0; count < new_length; count++) {
-        new_t += " ";
-    }
-
-    return new_t;
-}
-
-function fix_ip(neww) {
-    let new_t = "  " + neww;
-    let new_length = ip_length - neww.length - 2;
-    let count = 0;
-    for(count = 0; count < new_length; count++) {
-        new_t += " ";
-    }
-
-    return new_t;
-}
-
-function fix_level(neww) {
-    let new_t = "  " + neww;
-    let new_length = level_box - neww.length - 2;
-    let count = 0;
-    for(count = 0; count < new_length; count++) {
-        new_t += " ";
-    }
-
-    return new_t;
-}
+console.log(admin.show_users());
+// table = grid.CreateHeader();
+// let gay = fs.readFileSync("./db/users.db", "utf8");
+// let fix = gay.split("('").join("");
+// let fix2 = fix.split("')").join("");
+// let split = fix2.split("\n");
+// split.forEach(u => {
+//     if(u.length > 5) {
+//         let skidbag = u.split("','");
+//         let isAdmin = false;
+//         switch(parseInt(skidbag[5])) {
+//             case 0:
+//                 isAdmin = false;
+//                 break;
+//             case 1:
+//                 isAdmin = true;
+//                 break;
+//         }
+//         table += grid.CreateRow(skidbag[0], skidbag[1], skidbag[3], skidbag[4], isAdmin.toString());
+//     }
+// })
+// table += grid.CreateFooter();
+// console.log(table);
