7e21eac861cfe51461f328a97c62cd49e854d9e0
Devious <74839733+DeviousTheDev@users.noreply.github.com>
2021-01-25 18:44:50 -0500

    Update admin.js

--- auth/admin.js
+++ auth/admin.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
-const grid = require("../extra/data_grid.js");
+const grid = require("../banners/data_grid.js");
+const admingrid = require("../banners/animation.js");
 
 exports.show_users = function() {
     table = grid.CreateHeader();
@@ -24,4 +25,20 @@ exports.show_users = function() {
     })
     table += grid.CreateFooter();
     return table;
+}
+
+exports.show_current_users = function() {
+    table = admingrid.CreateHeader();
+    let gay = fs.readFileSync("./db/current.db", "utf8");
+    let fix = gay.split("('").join("");
+    let fix2 = fix.split("')").join("");
+    let split = fix2.split("\n");
+    split.forEach(u => {
+        if(u.length > 5) {
+            let skidbag = u.split("','");
+           table += admingrid.CreateRow(skidbag[0], skidbag[1]);
+       }
+   })
+   table += admingrid.CreateFooter();
+   return table;
 }
