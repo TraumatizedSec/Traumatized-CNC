8dcf84213ef37d0a6e86189361f9a8cddc096481
Devious <74839733+DeviousTheDev@users.noreply.github.com>
2021-01-25 13:21:43 -0500

    Update test.js

--- test.js
+++ test.js
@@ -1,4 +1,6 @@
 const fs = require("fs");
-const gay = require("./extra/functions.js");
+const func = require("./extra/functions.js");
+const crud = require("./auth/crud.js");
 
-console.log(gay.show_stats(process.argv[2]));
+// console.log(gay.show_stats(process.argv[2])); //SHOW STATS
+console.log(crud.update(process.argv[2], process.argv[3], process.argv[4], process.argv[5], process.argv[6])); // UPDATE USER
