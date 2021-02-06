ed0a536e375dea190cdbf8fddcfe98b3f2a9a777
Devious <74839733+DeviousTheDev@users.noreply.github.com>
2021-02-06 16:43:47 -0500

    Update animation.js

--- banners/animation.js
+++ banners/animation.js
@@ -11,16 +11,16 @@ exports.ip_length = gay.ip_box.length;
 exports.CreateHeader = function() {
     let final_table = "";
     final_table += config.Colors.Purple + "                       ╔═══════════════════════════════╗\r\n";
-    final_table += "                       ║          " + config.Colors.Yellow + "Online User"  + config.Colors.Purple + "          ║\r\n";
+    final_table += "                       ║          " + config.Colors.Cyan + "Online User"  + config.Colors.Purple + "          ║\r\n";
     final_table += "                       ╚════╦══╦══════════════╦══╦═════╝\r\n";
     final_table += "                       ╔════╝  ╚════╗  ╔══════╝  ╚═══════╗\r\n";
-    final_table += "                       ║    " + config.Colors.Yellow + "Name" + config.Colors.Purple + "    ║  ║       " + config.Colors.Yellow + "IP" + config.Colors.Purple + "        ║\r\n";
+    final_table += "                       ║    " + config.Colors.Cyan + "Name" + config.Colors.Purple + "    ║  ║       " + config.Colors.Cyan + "IP" + config.Colors.Purple + "        ║\r\n";
     final_table += "                       ╠════════════╣  ╠═════════════════╣\r\n";
     return final_table;
 }
 
 exports.CreateRow = function(name, ip) {
-    return "                       ║" + config.Colors.Yellow + gay.fix_name(name) + config.Colors.Purple + "║  ║" + config.Colors.Yellow + gay.fix_ip(ip) + config.Colors.Purple + "║\r\n";
+    return "                       ║" + config.Colors.Cyan + gay.fix_name(name) + config.Colors.Purple + "║  ║" + config.Colors.Cyan + gay.fix_ip(ip) + config.Colors.Purple + "║\r\n";
 }
 
 exports.CreateFooter = function() {
