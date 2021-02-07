52a6927a20fd67d6f9d797a1d946ea3040c14686
Devious <74839733+DeviousTheDev@users.noreply.github.com>
2021-02-07 15:43:48 -0500

    Update login.js

--- auth/login.js
+++ auth/login.js
@@ -10,11 +10,18 @@ exports.login = function(usr, pw, ip) {
         if(crud.isSignedIn(usr) === false || crud.isSignedIn(ip) === false) {
             let info = get_user.split(",");
             let db_username = info[0];
+            let db_ip = info[1];
             let db_pw = info[2];
             if(db_username === usr) {
                 if(db_pw === pw) {
-                    crud.log_session(usr, ip);
-                    return "Successfully logged in! Welcome: " + usr;
+                    if(db_ip === "none") {
+                        crud.update(ip, info[3], info[4], info[5]);
+                        crud.log_session(usr, ip);
+                        return "Successfully logged in! Welcome: " + usr;
+                    } else {
+                        crud.log_session(usr, ip);
+                        return "Successfully logged in! Welcome: " + usr;
+                    }
                 } else {
                     return "2 Username or password seems to be incorrect!";
                 }
